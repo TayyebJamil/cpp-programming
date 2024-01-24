45d9e9f4337a9b6cdb03ec204d9709512e06546c
TayyebJamil <its.tayyeb.ch159@gmail.com>
2024-01-24 15:03:52 +0500

    calculator + ascii

---
+++ 03simple calculator.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std ;
+
+int main ()
+{
+	int a,b;
+	int sum;
+	cout <<"Type an integer number = ";
+	cin>>a;
+	cout<<"Type another integer number = ";
+	cin>>b;
+	sum = a+b;
+	cout<<"Sum of your numbers is = "<<sum;
+	
+}
