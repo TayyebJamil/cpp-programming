ac6a9a84bde69dd0b852d947b776ec6f5c6986fa
TayyebJamil <its.tayyeb.ch159@gmail.com>
2024-01-24 15:52:02 +0500

    adding programs

---
+++ 06conditions.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a = 11, b = 15;
+	if(a>b)
+	{
+		cout<<"A is less than b";
+	}
+	else
+	{
+		cout<<"A is greater than b";
+	}
+	
+	
+}
