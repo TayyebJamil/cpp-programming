702f8df7a9c00851c267db33c67d938269c8e7ce
TayyebJamil <its.tayyeb.ch159@gmail.com>
2024-01-26 15:50:57 +0500

    There is a programming language with only four operations and one variable X:  ++X and X++ increments the value of the variable X by 1. --X and X-- decrements the value of the variable X by 1. Initially, the value of X is 0.  Given an array of strings operations containing a list of operations, return the final value of X after performing all the operations.

---
+++ 18find value of variable after operations.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+	string operations[10];
+	int n;
+	cout<<"Enter number of operations = ";
+	cin>>n;
+	cout<<"Enter operations : (x++, ++x, x--, --x)"<<endl;
+	for(int i=0; i<n; i++){
+		cin>>operations[i];
+	}
+	
+	int x=0;
+	for(int i=0; i<n; i++){
+		if(operations[i]=="x++"||operations[i]=="x++"){
+			x++;
+		}
+		else if(operations[i]=="x--"||operations[i]=="--x"){
+			x--;
+		}
+		else{
+			cout<<"Your given operation is not valid ";
+		}
+	}
+	
+	cout<<"Value of x = "<<x;
+	
+}
