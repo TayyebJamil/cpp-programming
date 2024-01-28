5a92cf8d7eee9f97fb006adc199b53671a83ea69
TayyebJamil <its.tayyeb.ch159@gmail.com>
2024-01-28 22:13:10 +0500

    update

--- 20Linear search of an array.cpp
+++ 20Linear search of an array.cpp
@@ -1,33 +1,28 @@
 #include <iostream>
 using namespace std;
 
-int prime(int n);
-
-int main()
-{
-    int num, flag = 0;
-    cout << "Enter positive integer to check: ";
-    cin >> num;
-
-    // Argument num is passed to check() function
-    flag = prime(num);
-
-    if(flag == 1)
-        cout << num << " is not a prime number.";
-    else
-        cout<< num << " is a prime number.";
-    return 0;
+int linearsearch(int n, int arr[], int key){
+	for(int i=0; i<n; i++){
+		if(arr[i]==key){
+			return i;
+		}
+	}
+	return  -1;
 }
 
-/* This function returns integer value.  */
-int prime(int n)
-{
-    int i;
-    for(i = 2; i <= n/2; ++i)
-    {
-        if(n % i == 0)
-            return 1;
-    }
-
-    return 0;
+int main(){
+	int n;
+	cout<<"Enter number of elements :";
+	cin>>n;
+	cout<<"Enter elements of an array :";
+	int arr[n];
+	for(int i=0; i<n; i++){
+		cin>>arr[i];
+	}
+	
+	int key;
+	cout<<"Enter search key :";
+	cin>>key;
+	
+	cout<<linearsearch(n, arr, key);
 }
